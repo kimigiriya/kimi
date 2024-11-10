9d46a20880d43af135dddd968fbc43b15bc6cc47
kimigiriya <kimigiriya@gmail.com>
2024-11-10 19:26:35 +0700

    Add files via upload

---
+++ Complex/Complex.cpp
@@ -0,0 +1,61 @@
+﻿#include "Complex.h"
+#include <iostream>
+#include <stdio.h>
+#include <cmath>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    Complex c1(2.2, 3.9), c2(1.1, -2.6), c3;
+
+    std::cout << "\nПеременные с1 и с2\n";
+    std::cout << "c1: " << c1 << "\n";
+    std::cout << "c2: " << c2 << "\n";
+
+    std::cout << "\nСложение с1 и с2\n";
+    c3 = c1 + c2;
+    std::cout << "c1 + c2 = " << c1 << " + " << c2 << " = " << c3 << "\n";
+
+    std::cout << "\nВычитание с1 и с2\n";
+    c3 = c1 - c2;
+    std::cout << "c1 - c2 = " << c1 << " - " << c2 << " = " << c3 << "\n";
+
+    std::cout << "\nУмножение с1 и с2\n";
+    c3 = c1 * c2;
+    std::cout << "c1 * c2 = " << c1 << " * " << c2 << " = " << c3 << "\n";
+
+    std::cout << "\nДеление с1 и с2\n";
+    c3 = c1 / c2;
+    std::cout << "c1 / c2 = " << c1 << " / " << c2 << " = " << c3 << "\n";
+
+    std::cout << "\nПрефиксный инкремент с1\n";
+    ++c1;
+    std::cout << "++c1 = " << c1 << "\n";
+
+    std::cout << "\nПостфиксный инкремент с2\n";
+    c2++;
+    std::cout << "c2++ = " << c2 << "\n";
+
+    std::cout << "\nПрефиксный декремент с1\n";
+    --c1;
+    std::cout << "--c1 = " << c1 << "\n";
+
+    std::cout << "\nПостфиксный декремент с2\n";
+    c2--;
+    std::cout << "c2-- = " << c2 << "\n";
+
+    // Сравнение по модулю
+    std::cout << "\nСравнение по модулю с\n";
+    std::cout << "c1 > c2: " << (c1 > c2) << "\n";
+    std::cout << "c1 < c2: " << (c1 < c2) << "\n";
+    std::cout << "c1 == c2: " << (c1 == c2) << "\n";
+    std::cout << "c1 != c2: " << (c1 != c2) << "\n";
+
+    // Ввод комплексного числа
+    std::cout << "\nВведите комплексное число (real imag): ";
+    std::cin >> c3;
+    std::cout << "c3 = " << c3 << "\n";
+
+    return 0;
+}
---
+++ Complex/Complex.h
@@ -0,0 +1,124 @@
+﻿#pragma once
+
+#include <iostream>
+#include <stdio.h>
+#include <cmath>
+
+// Класс комплексных чисел
+class Complex {
+private:
+    double real;
+    double imag;
+
+public:
+    // Конструктор по умолчанию
+    Complex() : real(0.0), imag(0.0) {}
+
+    // Конструктор с параметрами
+    Complex(double r, double i) : real(r), imag(i) {}
+
+    // Конструктор копирования
+    Complex(const Complex& other) : real(other.real), imag(other.imag) {}
+
+    // Операция присваивания
+    Complex& operator=(const Complex& other) {
+        if (this != &other) {
+            real = other.real;
+            imag = other.imag;
+        }
+        return *this;
+    }
+
+    // Операция сложения
+    Complex operator+(const Complex& other) const {
+        return Complex(real + other.real, imag + other.imag);
+    }
+
+    // Операция вычитания
+    Complex operator-(const Complex& other) const {
+        return Complex(real - other.real, imag - other.imag);
+    }
+
+    // Операция умножения
+    Complex operator*(const Complex& other) const {
+        return Complex(real * other.real - imag * other.imag,
+            real * other.imag + imag * other.real);
+    }
+
+    // Операция деления
+    Complex operator/(const Complex& other) const {
+        double denominator = pow(other.real, 2) + pow(other.imag, 2);
+        return Complex((real * other.real + imag * other.imag) / denominator,
+            (imag * other.real - real * other.imag) / denominator);
+    }
+
+    // Префиксный инкремент
+    Complex& operator++() {
+        real++;
+        return *this;
+    }
+
+    // Постфиксный инкремент
+    Complex operator++(int) {
+        Complex temp(*this);
+        real++;
+        return temp;
+    }
+
+    // Префиксный декремент
+    Complex& operator--() {
+        real--;
+        return *this;
+    }
+
+    // Постфиксный декремент
+    Complex operator--(int) {
+        Complex temp(*this);
+        real--;
+        return temp;
+    }
+
+    // Операция сравнения ">" по модулю
+    bool operator>(const Complex& other) const {
+        return abs() > other.abs();
+    }
+
+    // Операция сравнения "<" по модулю
+    bool operator<(const Complex& other) const {
+        return abs() < other.abs();
+    }
+
+    // Операция сравнения "==" по модулю
+    bool operator==(const Complex& other) const {
+        return abs() == other.abs();
+    }
+
+    // Операция сравнения "!=" по модулю
+    bool operator!=(const Complex& other) const {
+        return abs() != other.abs();
+    }
+
+    // Получение модуля комплексного числа
+    double abs() const {
+        return sqrt(real * real + imag * imag);
+    }
+
+    // Дружественная функция ввода
+    friend std::istream& operator>>(std::istream& is, Complex& c);
+
+    // Дружественная функция вывода
+    friend std::ostream& operator<<(std::ostream& os, const Complex& c);
+};
+
+// Определение дружественной функции ввода
+std::istream& operator>>(std::istream& inside, Complex& c) {
+    inside >> c.real >> c.imag;
+    return inside;
+}
+
+// Определение дружественной функции вывода
+std::ostream& operator<<(std::ostream& outside, const Complex& c) {
+    outside << c.real << " + " << c.imag << "i";
+    return outside;
+}
+
